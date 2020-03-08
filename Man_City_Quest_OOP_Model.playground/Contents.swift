9f1635fc0df2c1a1dd88240d53524e01bb0569a3
allwak <oksamytny@phystech.edu>
2020-03-08 20:13:31 +0200

    Update methods

--- Man_City_Quest_OOP_Model.playground/Contents.swift
+++ Man_City_Quest_OOP_Model.playground/Contents.swift
@@ -28,7 +28,7 @@ protocol QuizzQuestionProtocol {
 public func true_false_randomizer() -> Bool {
     let number = Int.random(in: 0 ..< 10)
     
-    if number <= 4 {
+    if number <= 8 {
         return true
     } else {
         return false
@@ -82,39 +82,55 @@ class QuizzQuestion: QuizzQuestionProtocol {
 class QuizzQuestionLevelOne: QuizzQuestion {
     
     var previousQuestion: QuizzQuestion?
+    var mainQuestionValue = 10
+    var mainQuestionFakeValue = -5
+    var additionalQuestionOneValue = 8
     var localMainValue = 0
     var localAdditionalValue = 0
+    var localAnswer : Bool
+    var localFake : Bool
 
     override init() {
+        self.localAnswer = Bool()
+        self.localFake = Bool()
         //self.previousQuestion = previousQuestion!
-    }
-
-    func setValues() {
-        self.mainQuestion.value = 10
-        self.mainQuestion.fakeValue = -5
-        self.additionalQuestionOne?.value = 8
     }
     
     func checkPreviousQuestion() {
         if self.previousQuestion != nil {
             if self.previousQuestion?.passedStatus == false {
                 self.passedStatus = false
-                return (localMainValue,localAdditionalValue) // (0, 0), status: false
+                print("Current status:", self.passedStatus)
             }
         }
     }
+
+    func setValues() {
+        self.mainQuestion.value = mainQuestionValue
+        self.mainQuestion.fakeValue = mainQuestionFakeValue
+        self.additionalQuestionOne?.value = additionalQuestionOneValue
+        self.mainQuestion.answer = true_false_randomizer()
+        localAnswer = self.mainQuestion.answer
+        print("Answer: ", localAnswer)
+        self.mainQuestion.fake = true_false_randomizer()
+        localFake = self.mainQuestion.fake
+        self.passedStatus = true
+    }
+    
+    func checkMainQuestion() {
+ 
+    }
     
     func getValues() -> (Int, Int) {
- 
         
+        checkPreviousQuestion()
 
-        
-        self.mainQuestion.answer = true_false_randomizer()
-        let localAnswer = self.mainQuestion.answer
-        self.mainQuestion.fake = true_false_randomizer()
-        let localFake = self.mainQuestion.fake
+        if self.passedStatus == false {
+            return (localMainValue,localAdditionalValue) // (0, 0), status: false
+        }
 
-       
+        setValues()
+
         if localAnswer {
             localMainValue = 10;
             self.passedStatus = true
@@ -151,78 +167,37 @@ class QuizzQuestionLevelTwo: QuizzQuestion {
     self.previousQuestion = previousQuestion
    }
    
-   func getValues() -> (Int, Int) {
+}
 
-       var localMainValue = 0
-       var localAdditionalValue = 0;
-
-       if self.previousQuestion.passedStatus == false {
-           self.passedStatus = false
-           return (localMainValue,localAdditionalValue) // (0, 0), status: false
-       }
-       
-       self.mainQuestion.answer = true_false_randomizer()
-       let localAnswer = self.mainQuestion.answer
-       self.mainQuestion.fake = true_false_randomizer()
-       let localFake = self.mainQuestion.fake
-
-       self.passedStatus = false
-
-      
-       if localAnswer {
-           localMainValue = 10;
-           self.passedStatus = true
-           return (localMainValue, localAdditionalValue) // (10, 10), status: true
-       } else {
-           if localFake {
-               localMainValue = -5
-               self.additionalQuestionOne!.answer = true_false_randomizer()
-               let localAdditionalAnswer = self.additionalQuestionOne!.answer
-               
-               if localAdditionalAnswer {
-                   localAdditionalValue = 8
-                   self.passedStatus = true // (-5, 8), status: true
-               }
-               return(localMainValue, localAdditionalValue) // (0, 0), status: false
-                
-           } else {
-               localMainValue = 0
-               return (localMainValue,localAdditionalValue) // (0, 0), status: false
-           }
-       }
-       
-       }
-   }
-
+print("Question 1:")
 var question_1_0 = QuizzQuestionLevelOne()
 question_1_0.getValues()
 question_1_0.passedStatus
-
+print("Question 2:")
 var question_2_0 = QuizzQuestionLevelOne()
 question_2_0.previousQuestion = question_1_0
 question_2_0.previousQuestion?.passedStatus
 question_2_0.getValues()
 question_2_0.passedStatus
 
-var question_3_0 = QuizzQuestionLevelTwo(previousQuestion: question_2_0)
-question_3_0.previousQuestion.passedStatus
-question_3_0.getValues()
-question_3_0.passedStatus
-
-var question_4_0 = QuizzQuestionLevelTwo(previousQuestion: question_3_0)
-question_4_0.getValues()
-question_4_0.passedStatus
-var question_5_0 = QuizzQuestionLevelTwo(previousQuestion: question_4_0)
-question_5_0.getValues()
-question_5_0.passedStatus
-var question_6_0 = QuizzQuestionLevelTwo(previousQuestion: question_5_0)
-question_6_0.getValues()
-question_6_0.passedStatus
-var question_7_0 = QuizzQuestionLevelTwo(previousQuestion: question_6_0)
-question_7_0.getValues()
-question_7_0.passedStatus
-var question_8_0 = QuizzQuestionLevelTwo(previousQuestion: question_7_0)
-question_8_0.getValues()
-question_8_0.passedStatus
-
+//var question_3_0 = QuizzQuestionLevelTwo(previousQuestion: question_2_0)
+//question_3_0.previousQuestion.passedStatus
+//question_3_0.getValues()
+//question_3_0.passedStatus
+//
+//var question_4_0 = QuizzQuestionLevelTwo(previousQuestion: question_3_0)
+//question_4_0.getValues()
+//question_4_0.passedStatus
+//var question_5_0 = QuizzQuestionLevelTwo(previousQuestion: question_4_0)
+//question_5_0.getValues()
+//question_5_0.passedStatus
+//var question_6_0 = QuizzQuestionLevelTwo(previousQuestion: question_5_0)
+//question_6_0.getValues()
+//question_6_0.passedStatus
+//var question_7_0 = QuizzQuestionLevelTwo(previousQuestion: question_6_0)
+//question_7_0.getValues()
+//question_7_0.passedStatus
+//var question_8_0 = QuizzQuestionLevelTwo(previousQuestion: question_7_0)
+//question_8_0.getValues()
+//question_8_0.passedStatus
 
